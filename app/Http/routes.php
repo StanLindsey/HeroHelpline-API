c8ca1b5ba65d332b599a8324c51d8a0b60e92c2e
wdmtech <willdavidmurray@gmail.com>
2016-04-17 20:50:03 +0100

    Moar data and styling

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,7 +6,7 @@ $app->get('/', function () use ($app, $fileManager) {
     $lines = $fileManager->getData();
 
     return view('index')->with([
-        'title' => "Humans",
+        'title' => "&#127788;",
         'quote' => $lines->random(),
     ]);
 });
@@ -15,6 +15,12 @@ $app->get('/another', function () use ($app, $fileManager) {
     $lines = $fileManager->getData();
 
     return $lines->random();
+});
+
+$app->get('/all', function () use ($app, $fileManager) {
+    $lines = $fileManager->getData();
+
+    return json_encode($lines);
 });
 
 $app->put('/{data}', function () use ($app, $fileManager) {
@@ -29,24 +35,30 @@ class FileManager {
     public function __construct()
     {
         $this->dataFile = storage_path('data/data.txt');
+
+        if ( ! file_exists($this->dataFile)) {
+            return "No data file found";
+        }
+
     }
 
     public function getData() {
 
-        if (file_exists($this->dataFile)) {
+        $lines = collect();
 
-            $lines = collect();
-
-            foreach(file($this->dataFile, FILE_IGNORE_NEW_LINES) as $line) {
-                if(!empty(trim($line))) {
-                    $lines->push($line);
-                }
+        foreach(file($this->dataFile, FILE_IGNORE_NEW_LINES) as $line) {
+            if(!empty(trim($line))) {
+                $lines->push($line);
             }
-
-            return $lines;
         }
 
-        return "No data file found";
+        return $lines;
+    }
+    public function getLastUpdated() {
+        return \Carbon\Carbon::parse(filectime($this->dataFile));
+    }
+    public function getLastAccessed() {
+        return \Carbon\Carbon::parse(fileatime($this->dataFile));
     }
 }
 
